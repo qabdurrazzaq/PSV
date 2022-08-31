6ac858b6189bafa75b8e36bd2de7320f319b700d
qabdurrazzaq <qureshiabdurrazzaq2002@gmail.com>
2022-08-31 16:34:35 +0530

    Static Files Integrated

--- MyCuisine/MyCuisine/settings.py
+++ MyCuisine/MyCuisine/settings.py
@@ -119,7 +119,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR,"static","media")
+STATIC_ROOT = os.path.join(BASE_DIR,"static","static_root")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"static","static_files"),
+]
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
